da557e7b43729a29a3f3edba7f0728631b4918c1
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-05-16 14:52:35 +0545

    Frontend

--- bookingapp/src/pages/Todo.jsx
+++ bookingapp/src/pages/Todo.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import "./todo.css"
 
 const Todo = () => {
     return (
-        <div>Todo</div>
+        <div id="todoContainer">
+            <div id="todoBox">
+                <h3 className='text-white text-center py-4'>My Todos</h3>
+
+                <div id="addTodoComponent" className='container'>
+                    <input type="text" name="addTodo" id="addTodo" placeholder='Add Todo' />
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+
+
+                <div id="todos" className='container my-4'>
+                    <div className="alert pending my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                    <div className="alert pending my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                    <div className="alert pending my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                    <div className="alert completed my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                    <div className="alert pending my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
     )
 }
 
