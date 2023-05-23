0110db1e62acdb3bd5d864ebe50a7a1c667a2cb3
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-05-23 11:37:57 +0545

    Nav

--- bookingapp/src/App.js
+++ bookingapp/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Todo from './pages/Todo';
 import API from './pages/API';
+import Booking from './pages/Booking';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<API />} />
+        <Route path='/' element={<Booking />} />
+        <Route path='/api' element={<API />} />
         <Route path='/todo' element={<Todo />} />
         <Route path='/about' element={<h1>About Page</h1>} />
         <Route path='/login' element={<h1>Login Page</h1>} />
---
+++ bookingapp/src/pages/Booking.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarComponent from '../components/bookingComponents/NavbarComponent'
+
+const Booking = () => {
+    return (
+        <>
+            <NavbarComponent />
+        </>
+    )
+}
+
+export default Booking
